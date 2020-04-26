3479746385887f0cae4b92a077d3d4f8a78862c7
Md towhid <newtowhid1232@gmail.com>
2020-04-26 12:52:56 +0600

    Final PUCIEE_SB Commit

---
+++ PucIeee/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PucIeee.Controllers
+{
+    public class AboutController : Controller
+    {
+        public ActionResult AboutIeee()
+        {
+            return View();
+        }
+        public ActionResult AboutRegionTen()
+        {
+            return View();
+        }
+
+        public ActionResult IeeeBangladeshSection()
+        {
+            return View();
+        }
+
+        public ActionResult PubStudent()
+        {
+            return View();
+        }
+	}
+}
---
+++ PucIeee/Controllers/TeamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PucIeee.Controllers
+{
+    public class TeamController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult D()
+        {
+            return View();
+        }
+	}
+}
